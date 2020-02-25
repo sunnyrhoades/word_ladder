0f97aeca9c5c480ed25b11e5b4a63f1ae6c494a3
Sunny Rhoades <smrb2016@mymail.pomona.edu>
2020-02-25 04:42:40 -0800

    updated word_ladder.py

--- word_ladder.py
+++ word_ladder.py
@@ -28,14 +28,42 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     Whenever it is impossible to generate a word ladder between the two words,
     the function returns `None`.
     '''
+    import collections as cl
+    import copy
 
+    f = open('words5.dict')
+    dictionary = f.readlines()
+    
+    if start_word in dictionary:
+        dictionary.remove(start_word)
+
+#   if end_word not in dictionary:
+#       return 0
+
+    dictionary.append(end_word)
+    queue = cl.deque([[start_word, 1]])
+    leng = len(start_word)
+    ladder = []
+    while queue:
+        word, length = queue.popleft()
+        ladder.append(word)
+        if word == end_word:
+            return ladder
+        dictionary_copy = dictionary.copy()
+        for n in dictionary_copy:
+            if sum(n[i] != word[i] for i in range(leng)) == 1:
+                dictionary.remove(n)
+                queue.append([n, length+1])
+    return 0
 
 def verify_word_ladder(ladder):
     '''
     Returns True if each entry of the input list is adjacent to its neighbors;
     otherwise returns False.
     '''
-
+    for i in range(len(ladder) - 1):
+        if _adjacent(ladder[i], ladder[i+1]) == True:
+            return True
 
 def _adjacent(word1, word2):
     '''
@@ -47,3 +75,13 @@ def _adjacent(word1, word2):
     >>> _adjacent('stone','money')
     False
     '''
+    matches = []
+    if len(word1) == len(word2):
+        for i in range(len(word1)):
+            if word1[i] == word2[i]:
+                matches.append('1')
+
+    if len(matches) >= len(word1) - 1:
+        return True
+    else:
+        return False
